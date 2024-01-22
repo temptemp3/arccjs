3c5175291b0cd4e264f0d84342a99ac9816c3dde
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2024-01-22 09:08:07 -0700

    export zero address

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import CONTRACT from "./src/lib/contract.js";
-export { oneAddress } from "./src/utils/account.js";
+export { oneAddress, zeroAddress } from "./src/utils/account.js";
 export default CONTRACT;
